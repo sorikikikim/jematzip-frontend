1d83a547c41646a830f0672655d0c1cc4159f478
Sori Kim <73383923+sorikikikim@users.noreply.github.com>
2024-04-17 03:15:57 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+function App() {
+    return (
+        <div className="App">
+            안녕하세요 저는 김소리 입니다.
+            <img src="images/me.jpg" alt="내사진" />
+        </div>
+    );
+}
+
+export default App;
